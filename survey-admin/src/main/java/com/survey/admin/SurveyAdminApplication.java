1aeaea91e887c87be8f13e5a6fd6f39014254dca
EvilChan <1395822743@qq.com>
2026-09-02 21:06:21 +0800

    项目初始化

---
+++ survey-admin/src/main/java/com/survey/admin/SurveyAdminApplication.java
@@ -0,0 +1,13 @@
+package com.survey.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyAdminApplication.class);
+    }
+
+}
