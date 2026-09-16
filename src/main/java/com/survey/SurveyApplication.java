d99f1e6a605172c10f0e7769b3c9ed34c91a7627
EvilChan <1395822743@qq.com>
2026-09-16 17:51:37 +0800

    添加 mybatis-plus 库

--- src/main/java/com/survey/SurveyApplication.java
+++ src/main/java/com/survey/SurveyApplication.java
@@ -1,9 +1,14 @@
-package com.fairy.survey;
+package com.survey;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(
+	basePackages = "com.survey",
+	annotationClass = org.apache.ibatis.annotations.Mapper.class
+)
 public class SurveyApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/survey/SurveyApplicationTests.java
+++ src/test/java/com/survey/SurveyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fairy.survey;
+package com.survey;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
